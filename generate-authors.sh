2025934647ede3776a5daded373ed393e90a730a
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-11-11 18:12:38 +0000

    push authors script

--- generate-authors.sh
+++ generate-authors.sh
@@ -9,7 +9,7 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
 	cat <<- 'EOH'
 		# This file lists all individuals having contributed content to the repository.
 		# For how it is generated, see `hack/generate-authors.sh`.
-		EOH
-		echo
-		git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
+	EOH
+	echo
+	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
 } > AUTHORS
